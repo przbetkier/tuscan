ed096a68b860ed651f2f35ac47ec7091c7b469ea
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2019-07-26 22:53:23 +0200

    CORS Configuration. Plugin endpoint extracted to separate file

--- src/main/java/io/github/przbetkier/tuscan/config/SecurityConfiguration.java
+++ src/main/java/io/github/przbetkier/tuscan/config/SecurityConfiguration.java
@@ -22,6 +22,7 @@ public class SecurityConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/tuscan-api/plugin/**").allowedMethods("GET");
                 registry.addMapping("/**").allowedOrigins(tuscanFrontendUrl);
             }
         };
--- src/test/groovy/integration/adapter/api/PlayerDetailsEndpointIntegrationSpec.groovy
+++ src/test/groovy/integration/adapter/api/PlayerDetailsEndpointIntegrationSpec.groovy
@@ -3,7 +3,6 @@ package integration.adapter.api
 import integration.BaseIntegrationSpec
 import integration.common.stubs.PlayerCsgoStatsStubs
 import integration.common.stubs.PlayerDetailsStubs
-import io.github.przbetkier.tuscan.adapter.api.response.DetailedPlayerCsgoStatsResponse
 import io.github.przbetkier.tuscan.adapter.api.response.PlayerCsgoStatsResponse
 import io.github.przbetkier.tuscan.common.SamplePlayerDetails
 import spock.lang.Unroll
@@ -90,44 +89,5 @@ class PlayerDetailsEndpointIntegrationSpec extends BaseIntegrationSpec {
         banStartsAt                       || activeBan
         ZonedDateTime.now().minusHours(1) || true
         ZonedDateTime.now().plusHours(1)  || false
-    }
-
-    def "should get CSGO details for multiple players"() {
-        given:
-        def player1 = SamplePlayerDetails.simple("playerId-1", "player1")
-        def player2 = SamplePlayerDetails.simple("playerId-2", "player2")
-        PlayerDetailsStubs.stubSuccessfulResponse(player1)
-        PlayerDetailsStubs.stubSuccessfulResponse(player2)
-        PlayerCsgoStatsStubs.stubSuccessfulResponse(player1.playerId)
-        PlayerCsgoStatsStubs.stubSuccessfulResponse(player2.playerId)
-
-        when:
-        def response = restTemplate.getForEntity(localUrl("/faceit/players/details/csgo?nickname=${player1.nickname},${player2.nickname}"), DetailedPlayerCsgoStatsResponse[].class)
-
-        then:
-        response.statusCodeValue == 200
-        response.body.size() == 2
-        response.body.find({it.nickname == player1.nickname}) != null
-        response.body.find({it.nickname == player2.nickname}) != null
-        response.body.each {it.mapStats != null && it.overallStats != null}
-    }
-
-    def "should get CSGO details only for available players"() {
-        given:
-        def player1 = SamplePlayerDetails.simple("playerId-1", "player1")
-        def player2 = SamplePlayerDetails.simple("playerId-2", "player2")
-        PlayerDetailsStubs.stubSuccessfulResponse(player1)
-        PlayerDetailsStubs.stubNotFoundResponse(player2.nickname)
-        PlayerCsgoStatsStubs.stubSuccessfulResponse(player1.playerId)
-        PlayerCsgoStatsStubs.stubNotFound(player2.playerId)
-
-        when:
-        def response = restTemplate.getForEntity(localUrl("/faceit/players/details/csgo?nickname=${player1.nickname},${player2.nickname}"), DetailedPlayerCsgoStatsResponse[].class)
-
-        then:
-        response.statusCodeValue == 200
-        response.body.size() == 1
-        response.body.find({it.nickname == player1.nickname}) != null
-        response.body.find({it.nickname == player2.nickname}) == null
     }
 }
