985143e0f8fdd68333f75b460fa0d18157546892
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2019-09-28 17:32:08 +0200

    Player position mapper test

---
+++ src/test/groovy/io/github/przbetkier/tuscan/domain/player/PlayerPositionMapperTest.groovy
@@ -0,0 +1,22 @@
+package io.github.przbetkier.tuscan.domain.player
+
+import io.github.przbetkier.tuscan.client.player.Position
+import reactor.util.function.Tuple2
+import spock.lang.Specification
+
+class PlayerPositionMapperTest extends Specification {
+
+    def "should map to player position response"() {
+        given:
+        def playerId = "playerId"
+        def regionPosition = new Position("32119")
+        def countryPosition = new Position("3999")
+
+        when:
+        def response = PlayerPositionMapper.map(playerId, new Tuple2<Position, Position>(regionPosition, countryPosition))
+        then:
+        response.playerId == playerId
+        response.positionInRegion == 32119
+        response.positionInCountry == 3999
+    }
+}
