a02e38643c3d88be9cb3daca0a36effc1628aa8c
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2019-10-22 23:21:06 +0200

    #45 fix for 404 on PluginEndpoint when one of the players has no stats

--- src/test/groovy/integration/adapter/api/PlayerDetailsPluginEndpointIntegrationSpec.groovy
+++ src/test/groovy/integration/adapter/api/PlayerDetailsPluginEndpointIntegrationSpec.groovy
@@ -28,12 +28,31 @@ class PlayerDetailsPluginEndpointIntegrationSpec extends BaseIntegrationSpec {
         response.body.each {it.mapStats != null && it.overallStats != null}
     }
 
-    def "should get CSGO details only for available players"() {
+    def "should get CSGO details only for players that still exist"() {
         given:
         def player1 = SamplePlayerDetails.simple("playerId-1", "player1")
         def player2 = SamplePlayerDetails.simple("playerId-2", "player2")
         PlayerDetailsStubs.stubSuccessfulResponse(player1)
         PlayerDetailsStubs.stubNotFoundResponse(player2.nickname)
+        PlayerCsgoStatsStubs.stubSuccessfulResponse(player1.playerId)
+        PlayerCsgoStatsStubs.stubNotFound(player2.playerId)
+
+        when:
+        def response = restTemplate.getForEntity(localUrl("/tuscan-api/plugin/players/details/csgo?nickname=${player1.nickname},${player2.nickname}"), DetailedPlayerCsgoStatsResponse[].class)
+
+        then:
+        response.statusCodeValue == 200
+        response.body.size() == 1
+        response.body.find({it.nickname == player1.nickname}) != null
+        response.body.find({it.nickname == player2.nickname}) == null
+    }
+
+    def "should get CSGO details only for players players with csgo stats"() {
+        given:
+        def player1 = SamplePlayerDetails.simple("playerId-1", "player1")
+        def player2 = SamplePlayerDetails.simple("playerId-2", "player2")
+        PlayerDetailsStubs.stubSuccessfulResponse(player1)
+        PlayerDetailsStubs.stubSuccessfulResponse(player2)
         PlayerCsgoStatsStubs.stubSuccessfulResponse(player1.playerId)
         PlayerCsgoStatsStubs.stubNotFound(player2.playerId)
 
