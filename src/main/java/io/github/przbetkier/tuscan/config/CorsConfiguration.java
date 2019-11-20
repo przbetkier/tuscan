579ce7647b6b79f1e0c59f8ddd2099bd115849a6
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2019-11-20 20:48:24 +0100

    Revert to previous cors config.

--- src/main/java/io/github/przbetkier/tuscan/config/CorsConfiguration.java
+++ src/main/java/io/github/przbetkier/tuscan/config/CorsConfiguration.java
@@ -1,20 +1,18 @@
 package io.github.przbetkier.tuscan.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
-@Profile("prod")
 class CorsConfiguration implements WebFluxConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("https://www.tuscan.pro/", "https://tuscan.pro/", "www.tuscan.pro", "tuscan.pro")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST")
                 .maxAge(3600);
     }
